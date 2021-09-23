af8412879b0d50f80571c7784db97096d99c7e85
EnzoCoding <91261860+EnzoCoding@users.noreply.github.com>
2021-09-23 21:03:17 +0200

    Update tryboinc.py

--- tryboinc.py
+++ tryboinc.py
@@ -4,6 +4,10 @@ import time
 from subprocess import call
 from os import environ
 
+#CODE IS UNFINISHED, THIS IS JUST A TEMPLATE TO WORK ON. UNTESTED.
+#Get your key from the IBM grid profile, and prepare it, or just replace ACCOUNT_KEY as your needs are and privacy permits.
+#The program will try to install boinc, then sleeps for 5 minutes to allow for installation time. Next it connects to the IBM grid and starts contributing.
+
 account_key = (os.environ.get('ACCOUNT_KEY'))
 
 try:
