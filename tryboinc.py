fabcde2f863228d8efa37be6b503eb5a9aaf5444
EnzoCoding <91261860+EnzoCoding@users.noreply.github.com>
2021-09-24 19:24:47 +0200

    Update tryboinc.py

--- tryboinc.py
+++ tryboinc.py
@@ -5,13 +5,13 @@ from subprocess import call
 from os import environ
 
 #CODE IS UNFINISHED, THIS IS JUST A TEMPLATE TO WORK ON. UNTESTED.
-#Get your key from the IBM grid profile, and prepare it, or just replace ACCOUNT_KEY as your needs are and privacy permits.
+#Get your key from the IBM grid profile, and prepare it according to the computing hosts you want to use, or just replace ACCOUNT_KEY as your needs are and privacy permits.
 #The program will try to install boinc, then sleeps for 5 minutes to allow for installation time. Next it connects to the IBM grid and starts contributing.
 
 account_key = (os.environ.get('ACCOUNT_KEY'))
 
 try:
-    call(['sudo', 'apt', 'install', 'boinc', 'y'],
+    call(['sudo', 'apt', 'install', 'boinc', '-y'],
          stdout=open(os.devnull,'wb'), stderr=STDOUT)
     time.sleep(360)
 except:
